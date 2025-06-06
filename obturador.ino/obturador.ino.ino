486c36c43c52b945e943a36bed926b906c24136c
Stalin Beltan <stalin.beltran2006@gmail.com>
2025-06-05 21:15:16 -0500

    leemos comandos show y hide via serial desde putty

--- obturador.ino/obturador.ino.ino
+++ obturador.ino/obturador.ino.ino
@@ -2,6 +2,7 @@
 int limiteInferior = 50; //50 es el minimo aceptado por el servo, aparentemente
 int tiempo = 400;
 int digitalInput = 100;
+String comando = "";
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,15 +11,27 @@ void setup() {
 }
 
 void loop() {
-  digitalInput = 250;
+  if (Serial.available()){
+    Serial.println("comando");
+    comando = Serial.readString();
+    Serial.println(comando);
+  }
 
+  delay(3000);
+  Serial.println("estamos adentro");
+
+  if (comando == "show"){
+    digitalInput = 250;
     //Serial.println(digitalInput);     // Print Digital input in Serial monitor
     analogWrite(3, digitalInput);
     delay(tiempo);
+  }
 
+  if (comando == "hide"){
     digitalInput = 190;
     //Serial.println(digitalInput);
     analogWrite(3, digitalInput);
-    delay(tiempo*10);
+    delay(tiempo);
+  }
 
 }
